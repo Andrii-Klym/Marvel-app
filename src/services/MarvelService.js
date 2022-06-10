00090fe7393c1de7414cfb32d2c8791181a8feca
Klym Andrii <klym.andriy14@gmail.com>
2022-06-10 23:45:42 +0300

    added load more characters

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -1,6 +1,8 @@
 class MarvelService {
     _apiBase = 'https://gateway.marvel.com:443/v1/public/';
     _apiKey = 'apikey=e624f866c06e7aafbb33ccd70fb076a8';
+    _baseOfSet = 210;
+
 
     getResource = async (url) => {
         let res = await fetch(url);
@@ -12,8 +14,8 @@ class MarvelService {
         return await res.json();
     }
 
-    getAllCharacters = async () => {
-        const res = await this.getResource(`${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`);
+    getAllCharacters = async (offset = this._baseOfSet) => {
+        const res = await this.getResource(`${this._apiBase}characters?limit=9&offset=${offset}&${this._apiKey}`);
         return res.data.results.map(this._transformCharacter);
     }
 
